882161af57f2412211adf633a17e01f08a043229
jayrugman <jasonrhardman@gmail.com>
2025-05-19 12:26:25 -0600

    added user input on whether to load saved state or not.

--- python_timer_stuff/save_current_state.py
+++ python_timer_stuff/save_current_state.py
@@ -8,7 +8,7 @@ def save_state(the_class):
     the_file = '.timer_saved_state.pkl'
     with open(the_file, 'wb') as file:
         dill.dump(the_class, file)
-    the_class.lastAction = f'saved state to {the_file}'
+    the_class.lastAction = f'{the_class.lastAction}. saved state to {the_file}'
 
 
 def load_saved():
@@ -22,11 +22,15 @@ def load_saved():
         if now - file_mod_time > timedelta(hours=8):
             os.remove(the_file)
             return None
-        # if it's not older than 8 hours, open it and load into the active script
-        with open(the_file, 'rb') as file:
-            the_class = dill.load(file)
-        the_class.lastAction = 'loaded state from file'
-        return the_class
+        # if it's not older than 8 hours, ask user if they want to load it
+        cont = input("Saved state found. Load state (yes/no)? ")
+        if cont in ['yes', 'YES', 'y', 'Yes', 'Y']:
+            with open(the_file, 'rb') as file:
+                the_class = dill.load(file)
+            the_class.lastAction = 'loaded state from file'
+            return the_class
+        else:
+            return None
     except FileNotFoundError:
         return None
 
